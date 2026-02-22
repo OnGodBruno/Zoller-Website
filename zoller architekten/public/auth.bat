ecb6803b024341a854e5b04561df9da09269be5c
Zoller Website Mitarbeiter <firepec@gmail.com>
2026-02-22 23:30:36 +0100

    Auto-commit: 22.02.2026 23:30:36,57

--- zoller architekten/public/auth.bat
+++ zoller architekten/public/auth.bat
@@ -2,19 +2,21 @@
 
 cd /d //server/files/Homepage/Website/Zoller-Website
 
+
 git add .
+
 
 git diff-index --quiet HEAD --
 
 if errorlevel 1 (
 
 	git commit -m "Auto-commit: %date% %time%"
-    
+
 	git push origin master
-    
+
 	echo Changes pushed successfully!
 ) else (
-    
+
 	echo No changes to commit.
 
 )
